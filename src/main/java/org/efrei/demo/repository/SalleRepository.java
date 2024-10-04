30a8f99e52270faa161f82f6a9b8402a5f051253
MathieuAudibert <mathieu.audibert27@gmail.com>
2024-10-04 16:26:28 +0200

    oui

--- src/main/java/org/efrei/demo/repository/SalleRepository.java
+++
@@ -1,8 +0,0 @@
-package org.efrei.demo.repository;
-
-import org.efrei.demo.models.Salle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SalleRepository extends JpaRepository<Salle, String> {
-
-}
--- src/main/java/org/efrei/demo/services/SalleService.java
+++
@@ -1,9 +0,0 @@
-package org.efrei.demo.services;
-
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class SalleService {
-
-}
