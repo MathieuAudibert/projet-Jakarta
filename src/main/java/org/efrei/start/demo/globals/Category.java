17a7280173c48c72a499d44bb8bd8b260f0a7a5e
MathieuAudibert <mathieu.audibert27@gmail.com>
2024-10-04 15:11:03 +0200

    eys

--- src/main/java/org/efrei/start/demo/globals/Category.java
+++
@@ -1,9 +0,0 @@
-package org.efrei.start.demo.globals;
-
-public enum Category {
-
-    SCIENCE_FICTION,
-    ACTION,
-    HORROR,
-    DRAMA
-}
--- src/main/java/org/efrei/start/demo/repository/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package org.efrei.start.demo.repository;
-
-import org.efrei.start.models.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, String> {
-
-}
